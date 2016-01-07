22f97226c6e41d2a0e666143f41a5e4e8f1dbca2
dmitryame <dmitry@custommode.com>
2016-01-07 10:04:22 -0500

    progress

--- index.android.js
+++ index.android.js
@@ -1,28 +1,96 @@
 'use strict';
 
-
 var React = require('react-native');
-
 var {
   AppRegistry,
-  StyleSheet,
+  AsyncStorage,
+  PickerIOS,
   Text,
-  View,
-  MapView,
+  View
 } = React;
 
-var TenTags = React.createClass({
-  render: function() {
+var PickerItemIOS = PickerIOS.Item;
+
+var STORAGE_KEY = '@AsyncStorageExample:key';
+var COLORS = ['red', 'orange', 'yellow', 'green', 'blue'];
+
+var BasicStorageExample = React.createClass({
+  componentDidMount() {
+    this._loadInitialState().done();
+  },
+
+  async _loadInitialState() {
+    try {
+      var value = await AsyncStorage.getItem(STORAGE_KEY);
+      if (value !== null){
+        this.setState({selectedValue: value});
+        this._appendMessage('Recovered selection from disk: ' + value);
+      } else {
+        this._appendMessage('Initialized with no selection on disk.');
+      }
+    } catch (error) {
+      this._appendMessage('AsyncStorage error: ' + error.message);
+    }
+  },
+
+  getInitialState() {
+    return {
+      selectedValue: COLORS[0],
+      messages: [],
+    };
+  },
+
+  render() {
+    var color = this.state.selectedValue;
     return (
-      <MapView style={styles.map}></MapView>
+      <View>
+        <Text>
+          {'Selected: '}
+          <Text style={{color}}>
+            {this.state.selectedValue}
+          </Text>
+        </Text>
+        <Text>{' '}</Text>
+        <Text onPress={this._removeStorage}>
+          Press here to remove from storage.
+        </Text>
+        <Text>{' '}</Text>
+        <Text>Messages:</Text>
+        {this.state.messages.map((m) => <Text key={m}>{m}</Text>)}
+      </View>
     );
-  }
+  },
+
+  async _onValueChange(selectedValue) {
+    this.setState({selectedValue});
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, selectedValue);
+      this._appendMessage('Saved selection to disk: ' + selectedValue);
+    } catch (error) {
+      this._appendMessage('AsyncStorage error: ' + error.message);
+    }
+  },
+
+  async _removeStorage() {
+    try {
+      await AsyncStorage.removeItem(STORAGE_KEY);
+      this._appendMessage('Selection removed from disk.');
+    } catch (error) {
+      this._appendMessage('AsyncStorage error: ' + error.message);
+    }
+  },
+
+  _appendMessage(message) {
+    this.setState({messages: this.state.messages.concat(message)});
+  },
 });
 
-var styles = StyleSheet.create({
-  map: {
-    flex: 1
-  }
-});
-
-AppRegistry.registerComponent('TenTags', () => TenTags);
+exports.title = 'AsyncStorage';
+exports.description = 'Asynchronous local disk storage.';
+exports.examples = [
+  {
+    title: 'Basics - getItem, setItem, removeItem',
+    render(): ReactElement { return <BasicStorageExample />; }
+  },
+];
+AppRegistry.registerComponent('TenTags', () => BasicStorageExample);
--- index.ios.js
+++ index.ios.js
@@ -1,28 +1,65 @@
 'use strict';
 
-
 var React = require('react-native');
+// var Parse = require('parse').Parse;
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
 
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  MapView,
+  AsyncStorage,
 } = React;
 
 var TenTags = React.createClass({
-  render: function() {
-    return (
-      <MapView style={styles.map}></MapView>
-    );
-  }
-});
+  getInitialState: function() {
+    return {
+      user: null
+    };
+  },
+  componentWillMount: function() {
+    //prod
+    // Parse.initialize("zoYLGIcwju9NnQJxX6Kg4zV839tdwHCc2qNWKQGu", "DeTVIq6dl8x2hVynylVJneaDcvoRZ9vb3SOF04TW");
+    //dev
+    Parse.initialize("C0caJoLjtwhdtE3tXywMPzragKn7NxwPze0iZEIl", "cvmyxydA8IWtHRASJadWeFKNtM41VsvmObfsvgKi");
 
-var styles = StyleSheet.create({
-  map: {
-    flex: 1
-  }
-});
+    Parse.User.currentAsync()
+    .then(
+      (user) => {
+        console.log("user is initialized... " + user);
+        this.setState({user: user});
+      },
+      (error) => {
+        console.log("creating a new session...");
+      });
+    },
+    render: function() {
+      if (!this.state.user) {
+        return (
+          <View style={styles.container}>
+            <Text>...loading...</Text>
+          </View>
+        );
+      }
 
-AppRegistry.registerComponent('TenTags', () => TenTags);
+      var username = this.state.user.get('username');
+
+      return (
+        <View style={styles.container}>
+          <Text>{username}</Text>
+        </View>
+      );
+    }
+  });
+
+  var styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  });
+
+  AppRegistry.registerComponent('TenTags', () => TenTags);
