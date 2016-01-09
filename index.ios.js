0780aa3d09066504df50624ee845063f7f335709
dmitryame <dmitry@custommode.com>
2016-01-08 21:05:16 -0500

    current location detected

--- index.ios.js
+++ index.ios.js
@@ -17,105 +17,104 @@ function randomString(length) {
   return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 };
 
-var username = null;
-var password = null;
-
-
 var TenTags = React.createClass({
-  async _loadInitialState() {
-    username = await AsyncStorage.getItem("USER_NAME");
-    password = await AsyncStorage.getItem("PASSWORD");
-  },
-  async _saveStateLocal(key, value) {
-    await AsyncStorage.setItem(key, value);
-  },
+
   getInitialState: function() {
     return {
-      user: null
+      user: null,
+      currentPosition: 'unknown'
     };
   },
+
   componentWillMount: function() {
-    var DeviceUUID = require("react-native-device-uuid");
-    DeviceUUID.getUUID().then((uuid) => {
-      console.log(uuid);
-    });
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var currentPosition = JSON.stringify(position);
+        this.setState({currentPosition});
+        console.log("currentPosition: " + currentPosition);
+      }
+      ,
+      (error) => alert(error.message),
+      {enableHighAccuracy: false, timeout: 20000, maximumAge: 90000}
+    );
 
     //prod
     // Parse.initialize("zoYLGIcwju9NnQJxX6Kg4zV839tdwHCc2qNWKQGu", "DeTVIq6dl8x2hVynylVJneaDcvoRZ9vb3SOF04TW");
     //dev
     Parse.initialize("C0caJoLjtwhdtE3tXywMPzragKn7NxwPze0iZEIl", "cvmyxydA8IWtHRASJadWeFKNtM41VsvmObfsvgKi");
-
     try {
       Parse.User.currentAsync()
       .then(
         (user) => {
-          if(!user) {
-            // try to sign in based on the local storage data
-            this._loadInitialState();
-            if(!username) { // no user previously stored in local storage, really need to sign up
-              user = new Parse.User();
+          if(user === null) {
+            user = new Parse.User();
 
-              username = randomString(36);
-              password = randomString(36);
+            var username = randomString(64);
+            var password = randomString(64);
 
-              user.set('username', username);
-              user.set('password', password);
-              user.signUp(null, {
-                success: (user) => {
-                  console.log("1 user is initialized... " + user);
-                  this._saveStateLocal('USER_NAME', username);
-                  this._saveStateLocal('PASSWORD', password);
-                  this.setState({user: user});
-                },
-                error: (user, error) => { this.setState({errorMessage: error.message}); }
-              });
-            } else {// sign in here
-              Parse.User.logIn(username, password, {
-                success: (user) => {
-                  console.log("2 user is initialized... " + user);
-                  this._saveStateLocal('USER_NAME', username);
-                  this._saveStateLocal('PASSWORD', password);
-                  this.setState({user: user});
-                },
-                error: (data, error) => { this.setState({ errorMessage: error.message }); }
-              });
-            }
+            user.set('username', username);
+            user.set('password', password);
+            user.signUp(null, {
+              success: (user) => {
+                console.log("1: user is initialized... " + user);
+                this.setState({user: user});
+              },
+              error: (user, error) => {
+                this.setState({errorMessage: error.message});
+                alert(error.message);
+              }
+            });
           };
-          console.log("3 user is initialized... " + user);
+          console.log("3: user is initialized... " + user);
           this.setState({user: user});
         },
         (error) => {
-          console.log("creating a new session...");
+          console.log("error creating a new session...");
+          alert(error.message);
         });
       } catch (error) {
+        console.log("error..." + error);
         this.setState({ errorMessage: error.message });
+        alert(error.message);
       }
     },
+
+    componentWillUnmount: function() {
+    },
+
     render: function() {
       if (!this.state.user) {
         return (
           <View style={styles.container}>
-            <Text>...loading...</Text>
-          </View>
-        );
-      }
-
-      var username = this.state.user.get('username');
-
-      return (
-        <View style={styles.container}>
-          <Text>{username}</Text>
+          <Text>...loading...</Text>
         </View>
       );
     }
-  });
 
-  var styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-  });
+    var username = this.state.user.get('username');
 
-  AppRegistry.registerComponent('TenTags', () => TenTags);
+    return (
+      <View style={styles.container}>
+      <Text>{username}</Text>
+      <Text>
+            <Text style={styles.title}>Current position: </Text>
+            {this.state.currentPosition}
+      </Text>
+    </View>
+  );
+}
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+   fontWeight: '500',
+  }
+});
+
+AppRegistry.registerComponent('TenTags', () => TenTags);
